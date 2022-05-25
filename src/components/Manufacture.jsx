8b1b5fc5f14c1a18dd3657600a880194318e0ec5
Ayushi0516 <ayushigupta0516@gmail.com>
2022-05-25 22:35:11 +0530

    op done

---
+++ src/components/Manufacture.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import ReactDOM from 'react-dom/client';
+const Manufacture =() =>{
+return(
+    <>
+    <h1>Mobile Manufacturers</h1>
+    <ul className="list">
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+    </ul>
+    </>
+)
+};
+
+export  default Manufacture;
---
+++ src/components/Op.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import ReactDOM from 'react-dom/client';
+const Op =() =>{
+return(
+    <>
+    <h1>Mobile operating System</h1>
+    <ul className="list">
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iphone</li>
+        <li>Window phones</li>
+    </ul>
+    </>
+)
+};
+
+export  default Op;
